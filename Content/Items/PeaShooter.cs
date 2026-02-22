a9974b8d9d783df78eb9e5b8e4a154850e004947
daemonius <daemonius@DESKTOP-24M1CUR>
2026-02-22 22:06:20 +1100

    bug fig

--- Content/Items/PeaShooter.cs
+++ Content/Items/PeaShooter.cs
@@ -4,19 +4,20 @@ using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 using TheSludgeMod.Content.Items;
+using TheSludgeMod.Content.Projectiles;
 
 namespace TheSludgeMod.Content.Items
 {
-    public class XShotBlaster : ModItem
+    public class PeaShooter : ModItem
     {
         public override void SetDefaults()
         {
-            Item.damage = 19;
+            Item.damage = 45;
             Item.DamageType = DamageClass.Ranged;
             Item.width = 40;
             Item.height = 20;
-            Item.useTime = 12;
-            Item.useAnimation = 12;
+            Item.useTime = 5;
+            Item.useAnimation = 5;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.noMelee = true;
             Item.knockBack = 4f;
@@ -24,29 +25,8 @@ namespace TheSludgeMod.Content.Items
             Item.rare = ItemRarityID.LightRed;
             Item.UseSound = SoundID.Item11;
             Item.autoReuse = true;
-            Item.shoot = ProjectileID.Bullet; 
-            Item.shootSpeed = 16f;
-            Item.useAmmo = AmmoID.Bullet;
-        }
-        public override bool CanConsumeAmmo(Item ammo, Player player)
-        {
-            return Main.rand.NextFloat() >= 0.5f; 
-        }
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-        {
-            Vector2 offset = velocity.RotatedBy(MathHelper.PiOver2);
-
-            offset.Normalize();
-
-            offset *= 4f; 
-
-            Vector2 position1 = position + offset;
-            Vector2 position2 = position - offset;
-
-            Projectile.NewProjectile(source, position1, velocity, type, damage, knockback, player.whoAmI);
-            Projectile.NewProjectile(source, position2, velocity, type, damage, knockback, player.whoAmI);
-
-            return false;
+            Item.shoot = ModContent.ProjectileType<Pea>(); 
+            Item.shootSpeed = 25f;
         }
     }
 }
