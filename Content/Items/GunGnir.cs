b973febb7ce67ca1f5ff2840f95ac051189dc045
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-02-23 21:17:41 +1100

    added gun-gnir

---
+++ Content/Items/GunGnir.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheSludgeMod.Content.Projectiles;
+
+namespace TheSludgeMod.Content.Items
+{
+    public class GunGnir : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 118;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 50;
+            Item.height = 28;
+            Item.useTime = 18;
+            Item.useAnimation = 18;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noMelee = true;
+            Item.knockBack = 7f;
+            Item.value = Item.sellPrice(gold: 5);
+            Item.rare = ItemRarityID.Yellow;
+            Item.UseSound = SoundID.Item11;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<GungnirBullet>();
+            Item.shootSpeed = 32f;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Gungnir);
+            recipe.AddIngredient(ItemID.IllegalGunParts);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.Register();
+        }
+    }
+}
