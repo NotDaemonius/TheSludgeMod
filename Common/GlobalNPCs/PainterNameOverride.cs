3705a289b0a4b22c7d7f906fba9317a35c23eb70
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-02-23 16:35:22 +1100

    Painter can now only be named 'Painter'

---
+++ Common/GlobalNPCs/PainterNameOverride.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheSludgeMod.Common.GlobalNPCs
+{
+    public class PainterNameOverride : GlobalNPC
+    {
+        public override void ModifyNPCNameList(NPC npc, List<string> nameList)
+        {
+            // Check if the NPC being spawned is the Painter
+            if (npc.type == NPCID.Painter)
+            {
+                // Clear the existing vanilla name list entirely
+                nameList.Clear();
+
+                // Add your custom names here
+                nameList.Add("Painter");
+            }
+        }
+    }
+}
