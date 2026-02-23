9432d19a5b53a6e8450e25e2538b649b2c4782c4
daemonius <daemonius@DESKTOP-24M1CUR>
2026-02-23 21:08:17 +1100

    Added new whips

---
+++ Content/Items/SilkRopeWhip.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using TheSludgeMod.Content.Projectiles;
+
+namespace TheSludgeMod.Content.Items
+{
+    public class SilkRopeWhip : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToWhip(ModContent.ProjectileType<SilkRopeWhipProjectile>(), 9, 2, 4);
+            Item.reuseDelay = 14;
+            Item.rare = ItemRarityID.White;
+            Item.channel = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().AddIngredient(ItemID.SilkRope, 100).AddTile(TileID.Loom);
+        }
+
+        public override bool MeleePrefix()
+        {
+            return true;
+        }
+    }
+}
