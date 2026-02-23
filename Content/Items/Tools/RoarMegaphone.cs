e66382b0a8aac7eee57bcba92ab8899bcfed7ce3
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-02-24 00:47:14 +1100

    Added roar megaphone

---
+++ Content/Items/Tools/RoarMegaphone.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace TheSludgeMod.Content.Items.Tools
+{
+    public class RoarMegaphone : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.useTime = 120;
+            Item.useAnimation = 120;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.noMelee = true;
+            Item.rare = ItemRarityID.Green;
+            Item.UseSound= SoundID.Roar;
+        }
+        public override void UseStyle(Player player, Rectangle heldItemFrame)
+        {
+            player.itemLocation.X -= 4f * player.direction;
+            player.itemLocation.Y += 10f;
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DemoniteBar, 2);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
