e61dab68da1a88caf678af28f697719352705f11
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-02-26 16:50:11 +1100

    added pebble and temporarily commented broken umbrella code

---
+++ Content/Projectiles/Weapons/PebbleProj.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheSludgeMod.Content.Projectiles.Weapons
+{
+    public class PebbleProj : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 8;
+            Projectile.height = 8;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Throwing;
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 300;
+            Projectile.aiStyle = ProjAIStyleID.ThrownProjectile;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            SoundEngine.PlaySound(SoundID.Dig, Projectile.position);
+
+            for (int i = 0; i < 10; i++)
+            {
+                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Stone, oldVelocity.X * -0.5f, oldVelocity.Y * -0.5f, 100, default, 1.5f);
+                Main.dust[dust].noGravity = true;
+            }
+
+            return true;
+        }
+    }
+}
