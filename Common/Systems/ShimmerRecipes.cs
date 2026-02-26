fa19d42dff13fe07b76b6a1e50bc46791d81a8d1
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-02-26 17:45:09 +1100

    added diamond sword

---
+++ Common/Systems/ShimmerRecipes.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheSludgeMod.Content.Items.Materials;
+using TheSludgeMod.Content.Items.Zinc;
+using TheSludgeMod.Content.Items.Aluminium;
+using TheSludgeMod.Content.Items.Iridium;
+using TheSludgeMod.Content.Tiles;
+using TheSludgeMod.Content.Items.Weapons;
+
+namespace TheSludgeMod.Common.Systems
+{
+    public class ShimmerRecipes : ModSystem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.ShimmerTransformToItem[ModContent.ItemType<DiamondBlade>()] = ModContent.ItemType<DiamondSword>();
+            ItemID.Sets.ShimmerTransformToItem[ModContent.ItemType<DiamondSword>()] = ModContent.ItemType<DiamondBlade>();
+        }
+    }
+}
