8d8e9b25cc0d8584140f2e69634294785f4e2b25
daemonius <daemonius@DESKTOP-24M1CUR>
2026-02-27 22:46:50 +1100

    my

---
+++ Content/Items/Weapons/GalaxyBlaster.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheSludgeMod.Content.Projectiles.Weapons;
+
+namespace TheSludgeMod.Content.Items.Weapons
+{
+    public class GalaxyBlaster : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 56;
+            Item.height = 26;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useTime = 5;
+            Item.useAnimation = 5;
+            Item.autoReuse = true;
+            Item.shootSpeed = 20;
+            Item.DamageType = DamageClass.Magic;
+            Item.damage = 47;
+            Item.knockBack = -1;
+            Item.crit = 6;
+            Item.mana = 2;
+            Item.rare = ItemRarityID.Pink;
+            Item.UseSound = SoundID.Item157;
+            Item.autoReuse = true;
+            Item.shoot = ProjectileID.GreenLaser;
+
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-4f, -2f);
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 28f;
+            Vector2 perp = Vector2.Normalize(new Vector2(velocity.Y, -velocity.X));
+
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position -= muzzleOffset;
+                position += perp * 8f * player.direction;
+            }
+        }
+    }
+}   
