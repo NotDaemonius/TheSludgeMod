db50ed31ac0ffe497ab4d9a6a16ce299b6406307
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-03-17 02:20:04 +1100

    added 20 prefixes

---
+++ Content/Prefixes/Piercing.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Prefixes
+{
+    public class Piercing : ModPrefix
+    {
+        public override PrefixCategory Category => PrefixCategory.Accessory;
+
+        public override float RollChance(Item item) => 5f;
+
+        public override bool CanRoll(Item item) => true;
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.21f;
+        }
+    }
+
+    public class PiercingTooltip : GlobalItem
+    {
+        public override void UpdateAccessory(Item item, Player player, bool hideVisual)
+        {
+            if (item.prefix != ModContent.PrefixType<Piercing>())
+                return;
+
+            player.GetArmorPenetration(DamageClass.Generic) += 4;
+        }
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (item.prefix == ModContent.PrefixType<Piercing>())
+            {
+                var line = new TooltipLine(Mod, "PrefixArmorPenetration", "+4 armor penetration")
+                {
+                    IsModifier = true
+                };
+
+                int researchIndex = tooltips.FindIndex(t => t.Name == "JourneyResearch");
+
+                if (researchIndex != -1)
+                {
+                    tooltips.Insert(researchIndex, line);
+                }
+                else
+                {
+                    tooltips.Add(line);
+                }
+            }
+        }
+    }
+}
---
+++ Content/Prefixes/Rigid.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Prefixes
+{
+    public class Rigid : ModPrefix
+    {
+        public override PrefixCategory Category => PrefixCategory.Ranged;
+
+        public override float RollChance(Item item)
+        {
+            return 5f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            damageMult *= 1.1f;
+            knockbackMult *= 1.1f;
+            shootSpeedMult *= 0.9f;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 0.95f;
+        }
+    }
+}
