d67388e7eb964e5d61daaa7f11be1c1f006ef4c1
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-03-16 15:13:06 +1100

    changed flute

--- Content/Items/Materials/Flute.cs
+++ Content/Items/Materials/Flute.cs
@@ -1,11 +1,6 @@
-using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
-using Terraria.GameContent;
 using Terraria.ModLoader;
-using TheSludgeMod.Content.Projectiles;
-using TheSludgeMod.Content.Tiles;
 
 namespace TheSludgeMod.Content.Items.Materials
 {
