b2a575336e7e706bcce6fd6a57288ebf043347ed
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-03-16 17:52:04 +1100

    added summon

---
+++ Content/Buffs/NeuralShardBuff.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace TheSludgeMod.Content.Buffs
+{
+    public class NeuralShardBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.buffTime[buffIndex] = 18000;
+
+            int projType = ModContent.ProjectileType<Content.Projectiles.Weapons.NeuralShardProj>();
+            bool minionExists = false;
+
+            for (int i = 0; i < Main.maxProjectiles; i++)
+            {
+                Projectile p = Main.projectile[i];
+                if (p.active && p.owner == player.whoAmI && p.type == projType)
+                {
+                    minionExists = true;
+                    break;
+                }
+            }
+
+            if (!minionExists)
+                player.DelBuff(buffIndex);
+            else
+                player.buffTime[buffIndex] = 18000;
+        }
+    }
+}
---
+++ Content/Items/Weapons/NeuralShardStaff.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheSludgeMod.Content.Items.Weapons
+{
+    public class NeuralShardStaff : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 18;
+            Item.DamageType = DamageClass.Summon;
+            Item.mana = 10;
+            Item.width = 32;
+            Item.height = 32;
+            Item.useTime = 36;
+            Item.useAnimation = 36;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.noMelee = true;
+            Item.knockBack = 3f;
+            Item.value = Item.buyPrice(gold: 10);
+            Item.rare = ItemRarityID.LightPurple;
+            Item.UseSound = SoundID.Item44;
+            Item.shoot = ModContent.ProjectileType<Projectiles.Weapons.NeuralShardProj>();
+            Item.shootSpeed = 10f;
+            Item.buffType = ModContent.BuffType<Buffs.NeuralShardBuff>(); // create a buff too
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source,
+            Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            player.AddBuff(Item.buffType, 2);
+            position = Main.MouseWorld;
+            return true;
+        }
+    }
+}
