9e2145bbaabc8c97efe42fa06a865372c1b9842b
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-03-19 03:53:03 +1100

    added glaive

---
+++ Content/Items/Weapons/Glaive.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheSludgeMod.Content.Items.Materials;
+using TheSludgeMod.Content.Projectiles.Weapons;
+
+namespace TheSludgeMod.Content.Items.Weapons
+{
+	public class Glaive : ModItem
+	{
+		public override void SetDefaults()
+		{
+			Item.damage = 93;
+			Item.width = 32;
+			Item.height = 32;
+			Item.useTime = 10;
+			Item.useAnimation = 10;
+			Item.crit = 16;
+			Item.knockBack = 5f;
+			Item.shootSpeed = 20f;
+			Item.value = Item.buyPrice(gold: 5);
+			Item.rare = ItemRarityID.Pink;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.useTurn = true;
+			Item.noUseGraphic = true;
+			Item.noMelee = true;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.shoot = ModContent.ProjectileType<GlaiveProj>();
+			Item.DamageType = DamageClass.Melee;
+		}
+		public override bool CanUseItem(Player player)
+        {
+            return player.ownedProjectileCounts[Item.shoot] < 3;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+            .AddIngredient(ModContent.ItemType<SoulofSpite>(), 10)
+            .AddIngredient(ItemID.HallowedBar, 5)
+            .AddIngredient(ItemID.LightDisc)
+            .AddTile(TileID.MythrilAnvil)
+            .Register();
+        }
+    }
+}
---
+++ Content/Projectiles/Weapons/GlaiveProj.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace TheSludgeMod.Content.Projectiles.Weapons
+{
+    public class GlaiveProj : ModProjectile
+    {
+        private const int MaxChains = 4;
+        private const float SearchRange = 600f;
+        private const float HomingStrength = 0.8f;
+        private const float MinSpeed = 20f;
+        private const int TrailLength = 8;
+        private const float TrailMaxAlpha = 0.6f;
+        private record struct ChainState(int ChainsLeft, int TargetNPC, HashSet<int> HitNPCs);
+        private static readonly Dictionary<int, ChainState> State = new();
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = TrailLength;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.WoodenBoomerang);
+            Projectile.width = 32;
+            Projectile.height = 32;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.tileCollide = false;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+            Vector2 origin = texture.Size() / 2f;
+
+            for (int i = 1; i < TrailLength; i++)
+            {
+                Vector2 drawPos = Projectile.oldPos[i] + new Vector2(Projectile.width, Projectile.height) / 2f - Main.screenPosition;
+                float progress = 1f - (float)i / TrailLength;
+                float alpha = TrailMaxAlpha * progress;
+                Color color = lightColor * alpha;
+                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.oldRot[i], origin, Projectile.scale, SpriteEffects.None, 0);
+            }
+
+            return true;
+        }
+        public override void OnSpawn(Terraria.DataStructures.IEntitySource source)
+        {
+            State[Projectile.whoAmI] = new ChainState(MaxChains, -1, new HashSet<int>());
+        }
+        public override void PostAI()
+        {
+            if (!State.TryGetValue(Projectile.whoAmI, out var s) || s.TargetNPC < 0)
+                return;
+
+            NPC target = Main.npc[s.TargetNPC];
+
+            if (!target.active)
+            {
+                State[Projectile.whoAmI] = s with { TargetNPC = -1 };
+                return;
+            }
+
+            Vector2 toTarget = target.Center - Projectile.Center;
+            float speed = MathHelper.Max(Projectile.velocity.Length(), MinSpeed);
+            Projectile.velocity = Vector2.Lerp(Projectile.velocity, toTarget.SafeNormalize(Vector2.Zero) * speed, HomingStrength);
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver4;
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (!State.TryGetValue(Projectile.whoAmI, out var s))
+                return;
+
+            s.HitNPCs.Add(target.whoAmI);
+
+            if (s.ChainsLeft <= 0)
+            {
+                State[Projectile.whoAmI] = s with { TargetNPC = -1 };
+                return;
+            }
+
+            NPC next = FindNearestUnhit(target.Center, s.HitNPCs);
+            int nextIndex = next?.whoAmI ?? -1;
+            State[Projectile.whoAmI] = new ChainState(s.ChainsLeft - 1, nextIndex, s.HitNPCs);
+        }
+        private static NPC FindNearestUnhit(Vector2 from, HashSet<int> alreadyHit)
+        {
+            NPC closest = null;
+            float closestDist = SearchRange * SearchRange;
+
+            foreach (NPC npc in Main.ActiveNPCs)
+            {
+                if (npc.friendly || npc.dontTakeDamage || npc.immortal)
+                    continue;
+
+                if (alreadyHit.Contains(npc.whoAmI))
+                    continue;
+
+                float dist = Vector2.DistanceSquared(from, npc.Center);
+
+                if (dist < closestDist)
+                {
+                    closestDist = dist;
+                    closest = npc;
+                }
+            }
+
+            return closest;
+        }
+        public override void OnKill(int timeLeft)
+        {
+            State.Remove(Projectile.whoAmI);
+        }
+    }
+}
