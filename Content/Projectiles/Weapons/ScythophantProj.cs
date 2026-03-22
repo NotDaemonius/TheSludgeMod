9b64046a73d918cc7439f34ad04b71d4fb1620a7
InfernoZXV <SebTvHdOfficial@gmail.com>
2026-03-22 17:58:30 +1100

    scythophant

--- Content/Projectiles/Weapons/ScythophantProj.cs
+++ Content/Projectiles/Weapons/ScythophantProj.cs
@@ -19,6 +19,7 @@ namespace TheSludgeMod.Content.Projectiles.Weapons
             Projectile.ignoreWater = true;
             Projectile.ownerHitCheck = true;
         }
+
         public override void AI()
         {
             Player player = Main.player[Projectile.owner];
@@ -32,11 +33,23 @@ namespace TheSludgeMod.Content.Projectiles.Weapons
             player.itemTime = 2;
             player.itemAnimation = 2;
             player.direction = (Main.MouseWorld.X > player.Center.X) ? 1 : -1;
+
+            if (Projectile.localAI[1] == 0f)
+            {
+                Projectile.localAI[0] = -MathHelper.PiOver2 + MathHelper.PiOver4;
+                Projectile.localAI[1] = 1f;
+            }
+
             float spinSpeed = 0.2f;
-            Projectile.rotation += spinSpeed;
+            Projectile.localAI[0] += spinSpeed * player.direction;
             float angleOffset = MathHelper.PiOver4;
-            float orbitRadius = 56f;
-            Vector2 offset = (Projectile.rotation - angleOffset).ToRotationVector2() * orbitRadius;
+            float orbitRadius = 48f;
+            float orbitAngle = Projectile.localAI[0];
+            player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation - float.Pi);
+            float visualOffset = (player.direction == -1) ? MathHelper.PiOver2 : 0f;
+            Projectile.rotation = orbitAngle + visualOffset;
+            Projectile.spriteDirection = player.direction;
+            Vector2 offset = (orbitAngle - angleOffset).ToRotationVector2() * orbitRadius;
             Projectile.Center = player.Center + offset;
             Projectile.ai[0]++;
             int fireRate = 6;
@@ -47,8 +60,8 @@ namespace TheSludgeMod.Content.Projectiles.Weapons
 
                 if (Main.myPlayer == Projectile.owner)
                 {
-                    Vector2 shootVelocity = (Projectile.rotation - angleOffset).ToRotationVector2() * 12f;
-                    Projectile.NewProjectile( Projectile.GetSource_FromThis(), Projectile.Center, shootVelocity, ModContent.ProjectileType<ScythophantProj2>(), Projectile.damage / 2, Projectile.knockBack * 0.5f, Projectile.owner);
+                    Vector2 shootVelocity = (orbitAngle - angleOffset).ToRotationVector2() * 12f;
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, shootVelocity, ModContent.ProjectileType<ScythophantProj2>(), Projectile.damage / 2, Projectile.knockBack * 0.5f, Projectile.owner);
                 }
             }
         }
