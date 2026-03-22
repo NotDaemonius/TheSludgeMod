4f288c82867611895ce7ac9d6dc6ab584c69e0e8
daemonius <daemonius@DESKTOP-24M1CUR>
2026-03-22 16:33:09 +1100

    Reworks

--- Common/HelperFunctions.cs
+++ Common/HelperFunctions.cs
@@ -1,24 +1,45 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
 
-namespace TheSludgeMod.Common
-{
-    public static class HelperFunctions
-    {
-        public static Vector2 AdjustMuzzleOffset(Player player, ref Vector2 position, Vector2 velocity, float intensity)
-        {
-            Vector2 muzzleOffset = Vector2.Normalize(velocity) * intensity;
+namespace TheSludgeMod.Common;
 
-            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+public static class HelperFunctions
+{
+    public static bool PlayerHasAccesory(Player player, short ItemID)
+    {
+        for (int i = 0; i < player.armor.Length; i++)
+        {
+            if (player.armor[i] != null && !player.armor[i].IsAir && player.armor[i].type == ItemID)
             {
-                position += muzzleOffset;
+                return true;
             }
-            return position;
         }
+
+        return false;
+    }
+
+    public static bool PlayerHasAccesory(Player player, int ItemID)
+    {
+        for (int i = 0; i < player.armor.Length; i++)
+        {
+            if (player.armor[i] != null && !player.armor[i].IsAir && player.armor[i].type == ItemID)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static Vector2 AdjustMuzzleOffset(Player player, ref Vector2 position, Vector2 velocity, float intensity)
+    {
+        Vector2 muzzleOffset = Vector2.Normalize(velocity) * intensity;
+
+        if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+        {
+            position += muzzleOffset;
+        }
+        return position;
     }
 }
+
